fc877353ddc3d1b6eebeb7ea7bad99f1827ad427
Marian Halibei <137928134+mar1anDEV@users.noreply.github.com>
2025-04-22 09:50:03 +0200

    vite.config.js aktualisieren

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  base: 'fmo-repo'
+  base: 'https://mar1andev.github.io/fmo-repo'
 })
